7d67b47707ce69d92cfaa5a7d24a8b115ea183ec
Dean Rickles <Dean@Rickles.co.uk>
2022-04-27 19:10:42 +0100

    Added PPIDreport_to_csv

--- README.md
+++ README.md
@@ -39,11 +39,13 @@ Starting with Sales and then add each catagory type after.
 
 ###### generateUrl.py
 
-generateURL
-    1) using  selected inputs manulate a URL for downloadrReport.
-- [x] (Sales) generateURL
-- [x] (Tender) generateURL
-- [x] (PPID) generateURL
+*Type*URL(vStartTimestamp,vEndTimeStamp,vPaymentMethod)
+
+    1) using  selected inputs manulate a breadcrumb URL for downloadReport.
+
+- [x] (Sales)   generateURL
+- [x] (Tender)  generateURL
+- [x] (PPID)    generateURL
 - [ ] (Recepts) generateURL
 - [ ] Review option to consolidate functions into one. 
 
@@ -51,33 +53,40 @@ generateURL
 ###### downloadReport.py
 
 Scrape CSV File.
+
         1) Open webbrowser
         2) Download report based on URL.
         3) Return list of file paths.
-- [x] (Sales)    Scrape CSV File. 
+
+- [x] (Sales)    Scrape CSV File.
     [24/04/2022] (Need to re-write to be generalised for import.)
-    [27/04/2022] ()
-- [ ] (Tender)   Scrape CSV File. 
-- [ ] (PPID)     Scrape CSV File. 
+- [x] (Tender)   Scrape CSV File. 
+    [27/04/2022] (Still requires testing.)
+- [x] (PPID)     Scrape CSV File. 
+    [27/04/2022] (Still requires testing.)
 - [ ] (Recepts)  Scrape CSV File. 
 
 
 ###### sql.py (postgres)
 
 Distinct Dates
+
     1) Open SQL connection.
     2) Read distinct dates the stored SQL.
     3) Return date list.
+
 - [x] (Sales)    Get Distinct Dates in SQL. [24/04/2022] (Requires re-writing for generalisation) 
 - [x] (Tender)   Get Distinct Dates in SQL. [24/04/2022] (Requires re-writing for generalisation)
 - [x] (PPID)     Get Distinct Dates in SQL. [24/04/2022] (Requires re-writing for generalisation)
 - [ ] (Reciepts) Get Distinct Dates in SQL.
 
 Import CSV to SQL
+
     1) Convert CSV into table.
     2) Open SQL connection.
     3) insert Table into SQL.
-- [ ] (Sales)    Import CSV into SQL.
-- [ ] (Tender)   Import CSV into SQL.
-- [ ] (PPID)     Import CSV into SQL.
-- [ ] (Recepts)  Import CSV into SQL.
+
+- [ ] (Sales)   Import CSV into SQL.
+- [ ] (Tender)  Import CSV into SQL.
+- [ ] (PPID)    Import CSV into SQL.
+- [ ] (Recepts) Import CSV into SQL.
--- myPayPoint/downloadReport.py
+++ myPayPoint/downloadReport.py
@@ -23,7 +23,7 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
 
-def SalesReport_to_CSV(*vURL, vSite, vUser, vPass,vPath):
+def SalesReport_to_CSV(*vURL, vSite, vUser, vPass, vPath):
     '''
         Arguments:
         vURL : URL of Paypoint
@@ -196,3 +196,360 @@ def SalesReport_to_CSV(*vURL, vSite, vUser, vPass,vPath):
         
         # Clean-up variables
         del vOutput,dataframe
+
+        
+def TenderReport_to_CSV(*vURL, vSite, vUser, vPass, vPath):
+    '''
+        Arguments:
+        vURL : URL of Paypoint
+        vSite: Site Code
+        vUser: username
+        vPass: password
+        vPath: Path of output /export/
+    ''' 
+    
+    #checks if path ends with a slash
+    if not vPath.endswith('/'):
+        vPath = vPath + '/'
+    
+    for file in glob.glob("/tmp/Tender Report*"):
+        try:
+            os.remove(file)
+        except:
+            print('Error while deleting file : ', file)    
+    
+    ''' Start Session 
+    '''
+    
+    # Geckodriver is required to make Selenium work.
+    # possible do a find / | grep geckodriver and select the first path?
+    s = Service('/usr/bin/geckodriver') 
+    
+    options = Options()
+    # Speed up load times by disabling options
+    options.set_preference("browser.tab.animate", 0)
+    options.set_preference("browser.panorama.animate_zoom", 0)
+    options.set_preference(" network.dns.disablePrefetch", True)
+    options.set_preference("network.prefetch-next ", False)
+    options.set_preference("network.http.speculative-parallel-limit", 0)
+    options.set_preference("permissions.default.image", 2)
+    options.set_preference("extensions.contentblocker.enabled", True)     
+    #options.set_preference("javascript.enabled", False)
+    options.set_preference("permissions.default.stylesheet", 2)
+    
+    # To prevent download dialog
+    options.set_preference("browser.download.folderList", 2)
+    options.set_preference("browser.download.manager.showWhenStarting", False)
+    options.set_preference("browser.download.dir", "/tmp")
+    #Example:profile.set_preference("browser.download.dir", "C:\Tutorial\down")
+    options.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/octet-stream")
+    
+    options.headless = True # headless browser running option (Critical option)
+    driver = webdriver.Firefox(service=s, options=options)  # load settings into firefox browser.
+    
+    ''' Login to Webpage
+    '''
+
+    # MAIN PAGE URL TO LOGIN.
+    driver.get('https://my.paypoint.com/login')
+    
+    # change to a wait for item on page with timeout.
+    time.sleep(10)
+    
+    # 'PayPoint Retailer Portal' = logged out.
+    # 'PayPoint EPoS' = logged in.
+    if driver.title == 'PayPoint Retailer Portal':
+        #print("Login Screen.")
+        # INPUT PASSWORD
+        driver.find_element(By.ID, "siteid_email").send_keys(vSite)
+        driver.find_element(By.ID, "username").send_keys(vUser)
+        driver.find_element(By.ID, "password").send_keys(vPass)
+        driver.find_element(By.CSS_SELECTOR, "#loginButton .pad-left").click()
+    else:
+        #print("Logged in.")
+        pass
+    
+    # change to a wait for tile on page with timeout.
+    time.sleep(5)
+    
+    # validation check
+    if driver.title != 'PayPoint Retailer Portal':
+        #driver.save_screenshot('ERROR_001.png')
+        sys.exit("ERROR: 001 Still on Login Screen. " + driver.title)
+    else:
+        print("Logged in.")
+    
+    
+    # connect to SQL
+    ''' Connect to Databasse
+    '''
+
+    # Connect to the database.
+    vConn = psycopg.connect(dbname=vServerDB, 
+                            user=vServerUser, 
+                            password=vServerPass, 
+                            host=vServerIP, 
+                            port=vServerPort)
+
+    # set the autocommit behavior of the current session.
+    vConn.autocommit = True
+
+    # Open a cursor to proform database operations.
+    vCurr = vConn.cursor()
+    
+    # converts turple into list.
+    if not isinstance(vURL, list):
+        vURL = list(vURL)
+        
+        #checks if list, is nested in a list.
+        if not isinstance(vURL[0],str):
+            vURL = vURL[0]
+    
+    ''' Loop through vURL
+    '''
+    
+    # loop through each instance.
+    for u in vURL:        
+        
+        j = json.loads(urlDecode(str(u.split('=')[1])))
+        # timestampStart_timestamp_end_saleType
+        vstartDate = j[1]['data']['startDate']
+        vendDate = j[1]['data']['endDate']
+        vPaymentMethod  = j[1]['name']
+        print(vPaymentMethod, 'on', vstartDate, 'to', vendDate, '- Loading.')
+    
+        
+        driver.get(u)
+        #print("Loading " + u)
+        # change to a wait for item on page with timeout.
+        time.sleep(5)
+        
+        # Select Export dropdown
+        driver.find_element(By.CSS_SELECTOR, ".btn-group:nth-child(1) > .btn").click()
+        print(vPaymentMethod, 'on', vstartDate, 'to', vendDate, "- Selecting Export Dropdown.")
+        # change to a wait for item on page with timeout.
+        time.sleep(5)
+        
+        # Select Export button
+        driver.find_element(By.LINK_TEXT, "Export to CSV").click()
+        print(vPaymentMethod, 'on', vstartDate, 'to', vendDate, "- Selecting Export Button.")
+
+        # wait for file to apear.
+        time.sleep(10)
+        
+        # local temp file path.
+        vFile = '/tmp/Tender Report.csv'
+        
+        # loads csv into memory.
+        csvData=pd.read_csv(vFile,
+                         encoding='latin1',
+                         header=1,
+                         index_col=False)
+        
+        # removes the temp report.
+        os.remove(vFile)
+        
+        # convert data into exportable format.
+        dataframe = pd.DataFrame(csvData) 
+        # adds startDate to data.
+        try:
+            csvData.insert(0,'startDate',vStartDate)
+        except:
+            pass
+        
+        # adds endDate to data.
+        try:
+            csvData.insert(1,'endDate',vEndDate)
+        except:
+            pass       
+        
+        # adds saleType to data.
+        try:
+            csvData.insert(2,'SaleType',vSaleType)
+        except:
+            pass
+        
+        # convert data into exportable format.
+        dataframe = pd.DataFrame(csvData)
+        
+        # export data to vPath.
+        # formart startdate_enddate_saletype.csv
+        vFileName = vStartDate.replace('/','').replace(':','').replace(' ','') + '_' + vEndDate.replace('/','').replace(':','').replace(' ','') + '_' + vSaleType.replace(' ','_') + '.csv'
+        
+        # export path fullname
+        vFullName = vPath + vFileName
+        
+        # export dataframe into csv format.
+        dataframe.to_csv(vFullName)
+        
+        # Clean-up variables
+        del vOutput,dataframe
+
+
+
+def PPIDReport_to_CSV(*vURL, vSite, vUser, vPass, vPath):
+    '''
+        Arguments:
+        vURL : URL of Paypoint
+        vSite: Site Code
+        vUser: username
+        vPass: password
+        vPath: Path of output /export/
+    ''' 
+    
+    #checks if path ends with a slash
+    if not vPath.endswith('/'):
+        vPath = vPath + '/'
+    
+    for file in glob.glob("/tmp/Tender Report*"):
+        try:
+            os.remove(file)
+        except:
+            print('Error while deleting file : ', file)    
+    
+    ''' Start Session 
+    '''
+    
+    # Geckodriver is required to make Selenium work.
+    # possible do a find / | grep geckodriver and select the first path?
+    s = Service('/usr/bin/geckodriver') 
+    
+    options = Options()
+    # Speed up load times by disabling options
+    options.set_preference("browser.tab.animate", 0)
+    options.set_preference("browser.panorama.animate_zoom", 0)
+    options.set_preference(" network.dns.disablePrefetch", True)
+    options.set_preference("network.prefetch-next ", False)
+    options.set_preference("network.http.speculative-parallel-limit", 0)
+    options.set_preference("permissions.default.image", 2)
+    options.set_preference("extensions.contentblocker.enabled", True)     
+    #options.set_preference("javascript.enabled", False)
+    options.set_preference("permissions.default.stylesheet", 2)
+    
+    # To prevent download dialog
+    options.set_preference("browser.download.folderList", 2)
+    options.set_preference("browser.download.manager.showWhenStarting", False)
+    options.set_preference("browser.download.dir", "/tmp")
+    #Example:profile.set_preference("browser.download.dir", "C:\Tutorial\down")
+    options.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/octet-stream")
+    
+    options.headless = True # headless browser running option (Critical option)
+    driver = webdriver.Firefox(service=s, options=options)  # load settings into firefox browser.
+    
+    ''' Login to Webpage
+    '''
+    
+    # MAIN PAGE URL TO LOGIN.
+    driver.get('https://my.paypoint.com/login')
+    
+    # change to a wait for item on page with timeout.
+    time.sleep(10)
+    
+    # 'PayPoint Retailer Portal' = logged out.
+    # 'PayPoint EPoS' = logged in.
+    if driver.title == 'PayPoint Retailer Portal':
+        #print("Login Screen.")
+        # INPUT PASSWORD
+        driver.find_element(By.ID, "siteid_email").send_keys(vSite)
+        driver.find_element(By.ID, "username").send_keys(vUser)
+        driver.find_element(By.ID, "password").send_keys(vPass)
+        driver.find_element(By.CSS_SELECTOR, "#loginButton .pad-left").click()
+    else:
+        #print("Logged in.")
+        pass
+    
+    # change to a wait for tile on page with timeout.
+    time.sleep(5)
+    
+    # validation check
+    if driver.title != 'PayPoint Retailer Portal':
+        #driver.save_screenshot('ERROR_001.png')
+        sys.exit("ERROR: 001 Still on Login Screen. " + driver.title)
+    else:
+        print("Logged in.")
+    
+    
+    # connect to SQL
+    ''' Connect to Databasse
+    '''
+
+    # Connect to the database.
+    vConn = psycopg.connect(dbname=vServerDB, 
+                            user=vServerUser, 
+                            password=vServerPass, 
+                            host=vServerIP, 
+                            port=vServerPort)
+
+    # set the autocommit behavior of the current session.
+    vConn.autocommit = True
+
+    # Open a cursor to proform database operations.
+    vCurr = vConn.cursor()
+    
+    # converts turple into list.
+    if not isinstance(vURL, list):
+        vURL = list(vURL)
+        
+        #checks if list, is nested in a list.
+        if not isinstance(vURL[0],str):
+            vURL = vURL[0]
+    
+    ''' Loop through vURL
+    '''
+    
+    # loop through each instance.
+    for u in vURL:        
+        
+        j = json.loads(urlDecode(str(u.split('=')[1])))
+        # timestampStart_timestamp_end_saleType
+        vstartDate = j[0]['data']['startDate']
+        vendDate = j[0]['data']['endDate']
+        vPaymentMethod  = 'PPID'
+        print(vPaymentMethod, 'on', vstartDate, 'to', vendDate, '- Loading.')
+    
+        
+        driver.get(u)
+        #print("Loading " + u)
+        # change to a wait for item on page with timeout.
+        time.sleep(5)
+        
+        # Select Export dropdown
+        driver.find_element(By.CSS_SELECTOR, ".btn-group:nth-child(1) > .btn").click()
+        print(vPaymentMethod, 'on', vstartDate, 'to', vendDate, "- Selecting Export Dropdown.")
+        # change to a wait for item on page with timeout.
+        time.sleep(5)
+        
+        # Select Export button
+        driver.find_element(By.LINK_TEXT, "Export to CSV").click()
+        print(vPaymentMethod, 'on', vstartDate, 'to', vendDate, "- Selecting Export Button.")
+
+        # wait for file to apear.
+        time.sleep(10)
+        
+        # local temp file path.
+        vFile = '/tmp/Pay Point Report.csv'
+        
+        # loads csv into memory.
+        csvData=pd.read_csv(vFile,
+                         encoding='latin1',
+                         header=1,
+                         index_col=False)
+        
+        # removes the temp report.
+        os.remove(vFile)
+        
+        # convert data into exportable format.
+        dataframe = pd.DataFrame(csvData)
+        
+        # export data to vPath.
+        # formart startdate_enddate_saletype.csv
+        vFileName = vStartDate.replace('/','').replace(':','').replace(' ','') + '_' + vEndDate.replace('/','').replace(':','').replace(' ','') + '_' + vPaymentMethod.replace(' ','_') + '.csv'
+        
+        # export path fullname
+        vFullName = vPath + vFileName
+        
+        # export dataframe into csv format.
+        dataframe.to_csv(vFullName)
+        
+        # Clean-up variables
+        del vOutput,dataframe,vPaymentMethod, vFullName, vPath, vFileName, csvData, vFile
